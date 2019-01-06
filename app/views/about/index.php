992b827362e9b83ff6fd2e4a5a6d3ba0eb0ec02d
Akbar Yusuf <40377648+akbr-ysf@users.noreply.github.com>
2019-01-07 02:14:32 +0700

    mvc-php assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
+<div class="container mt-4">
     <h1>Halaman Index</h1>
+    <img src="<?= BASEURL; ?>/img/profile.png" width="250" height="250" class="rounded-circle shadow">
     <p>Halo.. nama saya <?= $data['nama']?>, umur saya <?= $data['umur']?> tahun & saya adalah seorang <?= $data['pekerjaan']?>.</p>
+</div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -6,6 +6,22 @@
     <title>Halaman <?= $data['judul'];?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
     <script src="main.js"></script>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="<?= BASEURL; ?>">MVC PHP</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="<?= BASEURL; ?>/about">About</a>
+            </div>
+        </div>
+    </div>
+</nav>
