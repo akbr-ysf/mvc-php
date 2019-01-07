8fd4d0e5b49e49ae8c1ba238291b3b2e3f5c1c58
Akbar Yusuf <40377648+akbr-ysf@users.noreply.github.com>
2019-01-07 23:21:18 +0700

    mvc php insert data

--- app/views/Mahasiswa/index.php
+++ app/views/Mahasiswa/index.php
@@ -2,6 +2,51 @@
 
     <div class="row">
         <div class="col-6">
+            <button type="button" class="btn btn-primary mb-4" data-toggle="modal" data-target="#formModal">
+                Tambah Data Mahasiswa
+            </button>
+
+            <div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                                <div class="form-group">
+                                    <label for="nama">Nama</label>
+                                    <input type="text" class="form-control" id="nama" name="nama">
+                                </div>
+                                <div class="form-group">
+                                    <label for="nim">NIM</label>
+                                    <input type="number" class="form-control" id="nim" name="nim">
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" id="email" name="email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="jurusan">Jurusan</label>
+                                    <select class="form-control" id="jurusan" name="jurusan">
+                                        <option value="Komputer Akuntansi">Komputer Akuntansi</option>
+                                        <option value="Desain Grafis Multimedia & Film">Desain Grafis Multimedia & Film</option>
+                                        <option value="English For Tourism">English For Tourism</option>
+                                        <option value="Web Mobile Development">Web Mobile Development</option>
+                                    </select>
+                                </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Tambah Data</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
                 <?php foreach( $data['mhs'] as $mhs) : ?>   
